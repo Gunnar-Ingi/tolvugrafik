9a2faa913d89d5e77139007a5b7e50cbc6af5310
Gunnar Ingi <gunnaringi99@gmail.com>
2023-09-01 19:22:29 +0000

    daemi4

---
+++ site store/tri-slemb.js
@@ -0,0 +1,52 @@
+"use strict";
+
+var gl;
+let colorMap;
+
+
+window.onload = function init()
+{
+    var canvas = document.getElementById( "gl-canvas" );
+
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    var vertices = new Float32Array([-1, -1, 0, 1, 1, -1]);
+
+
+    //
+    //  Configure WebGL
+    //
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+
+    //  Load shaders and initialize attribute buffers
+
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+
+    // Load the data into the GPU
+
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW );
+
+    // Associate out shader variables with our data buffer
+
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    // random color hookup
+    colorMap = gl.getUniformLocation( program, "fColor")
+
+    render();
+};
+
+
+function render() {
+    gl.clear( gl.COLOR_BUFFER_BIT );
+
+    gl.uniform4fv(colorMap, vec4(Math.random(), Math.random(), Math.random(), 1.0));
+    gl.drawArrays( gl.TRIANGLES, 0, 3 );
+}
